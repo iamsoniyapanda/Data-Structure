ce273a9aa0e75ecc8f7cb5a891bcc58bb5719031
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-16 10:12:49 +0530

    Create 8.c

---
+++ Assigment 1/8.c
@@ -0,0 +1,18 @@
+//To chcek the sum of all elements of an array
+#include<stdio.h>
+int main()
+{
+  int a[5],sum=0;i;
+printf("Enter array elements");
+for(i-0;i<5;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Sum of array elements:");
+for(i=0;i<5;i++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum);
+return 0;
+}
