0f6805383780d8146b69013e7c72d54efc2bd143
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-16 09:58:13 +0530

    Create 4.c

---
+++ Assigment 1/4.c
@@ -0,0 +1,29 @@
+// A program to sort an array
+#include<stdio.h>
+int main()
+{
+  int a[5],i,j,temp;
+printf("Enter array elements");
+for(i=0;i<=5;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<5;i++)
+{
+for(j=i+1;j<5;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("Array elements:");
+for(i=0;i<5;i++)
+{
+printf("%d",a[i]);
+}
+return 0;
+}
