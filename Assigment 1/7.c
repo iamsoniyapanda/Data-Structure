0c4dd5da9541b5d5c9b2182a64fb4a4633add2fa
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-16 10:05:46 +0530

    Create 7.c

---
+++ Assigment 1/7.c
@@ -0,0 +1,16 @@
+//Reverse array
+#include<stdio.h>
+{
+int a[5],i;
+printf("Enter array elements:");
+for(i=0;i<4;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("\n Reverse array elements");
+for(i=4;i>=0;i--)
+{
+printf("%d"a[i]);
+}
+return 0;
+}
